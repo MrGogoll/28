9fd525e8f0dde66db10cb5d3075bb42046bdc959
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-20 15:52:40 +0300

    Add files via upload

---
+++ 333.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	string s;
+	int n;
+	cin >> s;
+	int t = s.length();
+	
+	if (t % 2 == 0) {
+		cout << "CHAT WITH HER!";
+	}
+	else {
+		cout << "IGNORE HIM!";
+	}
+	
+
+
+}
+
